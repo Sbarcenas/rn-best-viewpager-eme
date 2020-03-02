b05a701bd61794c7543662b9198a1183fe79755d
sbarcenas <SBarcenas_@outlook.com>
2020-03-02 12:29:31 -0500

    Page title cahnges

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
  * Created by Abbas on 2019/10/01.
  */
 
-'use strict';
+"use strict";
 
-import ViewPager from './viewpager/ViewPager';
-import IndicatorViewPager from './viewpager/IndicatorViewPager';
-import PagerDotIndicator from './viewpager/indicator/PagerDotIndicator';
-import PagerTitleIndicator from './viewpager/indicator/PagerTitleIndicator';
-import PagerTabIndicator from './viewpager/indicator/PagerTabIndicator';
+import ViewPager from "./viewpager/ViewPager";
+import IndicatorViewPager from "./viewpager/IndicatorViewPager";
+import PagerDotIndicator from "./viewpager/indicator/PagerDotIndicator";
+import PagerTitleIndicator from "./viewpager/indicator/PagerTitleIndicator";
+import PagerTabIndicator from "./viewpager/indicator/PagerT abIndicator";
 
 export {
-    ViewPager,
-    IndicatorViewPager,
-    PagerDotIndicator,
-    PagerTitleIndicator,
-    PagerTabIndicator
+  ViewPager,
+  IndicatorViewPager,
+  PagerDotIndicator,
+  PagerTitleIndicator,
+  PagerTabIndicator
 };
